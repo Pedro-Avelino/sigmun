edd72fe8ddf3255e4b54150aedb82226682a5422
Pepeu1805 <pedroavelino@alu.uern.br>
2022-07-18 16:36:47 -0300

    Tela de Profile Finalizada

---
+++ lib/resources/pages/profile/components/profile_menu.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sigmun/resources/themes/colors/colors.dart';
+
+class ProfileMenu extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Function()? onTap;
+
+  ProfileMenu(
+    this.text,
+    this.icon, {
+    this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        color: kLabelButtonColor,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        height: 81,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: <Widget>[
+                Icon(icon, color: kPrimaryColor),
+                SizedBox(width: 20),
+                Text(
+                  text,
+                  style: Theme.of(context).textTheme.button,
+                ),
+              ],
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: kPrimaryColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/resources/pages/profile/profile_page.dart
+++ lib/resources/pages/profile/profile_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 import '../../../app/controllers/profile_controller.dart';
 import 'package:sigmun/resources/themes/colors/colors.dart';
+import 'package:sigmun/resources/pages/profile/components/profile_menu.dart';
+import 'package:sigmun/routes/router.dart';
+import 'package:sigmun/resources/shared/label_button.dart';
+import 'package:sigmun/resources/themes/colors/colors.dart';
 
 class ProfilePage extends NyStatefulWidget {
   final ProfileController controller = ProfileController();
@@ -23,11 +27,68 @@ class _ProfilePageState extends NyState<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: kPrimaryColor),
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.close),
+              color: kPrimaryColor,
+              iconSize: 30,
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          SizedBox(height: 15),
+          Container(
+            child: Padding(
+              padding: EdgeInsets.symmetric(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'public/assets/images/profile_icon.png',
+                    height: 100,
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    'Pepeu',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '132.662.824-00',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  Text(
+                    'pedroavelino@alu.uern.br',
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Container(height: 1, color: kLineColor),
+          Column(
+            children: [
+              ProfileMenu(
+                'Editar dados do Perfil',
+                Icons.person_outline,
+                onTap: () {},
+              ),
+              ProfileMenu('Notificações', Icons.notifications_outlined),
+              ProfileMenu('Segurança', Icons.shield_outlined),
+              ProfileMenu('Ajuda', Icons.help_outline),
+              ProfileMenu('Fale Conosco', Icons.ring_volume),
+              ProfileMenu('Sobre', Icons.description_outlined),
+              ProfileMenu('Sair do Aplicativo', Icons.logout),
+            ],
+          ),
+          Container(height: 1, color: kLineColor),
+        ],
       ),
-      body: SafeArea(child: Container()),
     );
   }
 }
